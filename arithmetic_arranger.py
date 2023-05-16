915643a491de83f79c953fec66d32c71ee9211ab
Alanoterohs <65374890+Alanoterohs@users.noreply.github.com>
2023-05-16 02:45:35 +0000

    FCC-2
    main function added

---
+++ arithmetic_arranger.py
@@ -0,0 +1,12 @@
+import handleError
+
+
+def arithmetic_arranger(operations):
+  if handleError.checkLimit(operations):
+    return "Error: Too many problems."
+  elif handleError.checkOperators(operations):
+    return "Error: Operator must be '+' or '-'."
+  elif handleError.checkDigits(operations):
+    return "Error: Numbers must only contain digits."
+  elif (handleError.checkEachOperand(operations)):
+    return "Error: Numbers cannot be more than four digits."
