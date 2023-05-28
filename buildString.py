660da31254468e3f3af4e7acd8841bacf3b92336
Alanoterohs <65374890+Alanoterohs@users.noreply.github.com>
2023-05-28 03:20:14 +0000

    FCC - 4 - build lines

--- buildString.py
+++ buildString.py
@@ -1,16 +1,20 @@
-from re import split
+from re import split, findall
 
 
 def buildString(operations):
   matrizString = []
   index = 0
+  halfSizeMatriz = len(operations)
   buildMatriz(operations, 0, matrizString, index)
   buildMatriz(operations, 1, matrizString, index)
+  lineBuild(matrizString, halfSizeMatriz)
   print(matrizString)
+  #print(halfSizeMatriz)
 
 
 def buildMatriz(operations, mod, matrizString, index):
-  print(matrizString)
+  #print(matrizString)
+  global elementWithOperand
   for operation in operations:
     #print(operation)
     for number in split(r"[+|-]", operation):
@@ -24,3 +28,22 @@ def buildMatriz(operations, mod, matrizString, index):
       #else:
 
   #print(matrizString)
+
+
+def lineBuild(matrizString, halfSizeMatriz):
+  pattern = "[0-9]"
+  for operation in range(0, halfSizeMatriz):
+    sizeFirst = len(findall('\d+', matrizString[operation])[0])
+    sizeSecond = len(findall('\d+', matrizString[operation + halfSizeMatriz])[0])
+    if sizeFirst > sizeSecond:
+      matrizString.append(lines(sizeFirst + 2))
+    else:
+      matrizString.append(lines(sizeSecond + 2))
+
+
+def lines(size):
+  line = ""
+  for x in range(size):
+    line += "-"
+  print(line)
+  return line
